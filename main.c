905b9d84d1fde053c5962726f051144e7630cd89
Md. Rifat <jahidulislamrefat6@gmail.com>
2026-01-21 22:11:22 +0600

    Add switch for menu

--- main.c
+++ main.c
@@ -1,4 +1,8 @@
 #include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+
 typedef struct{
     int id;
     char name[50];
@@ -7,6 +11,92 @@ typedef struct{
 }Item;
 
 int main(){
+
+    char title[] = "**INVENTORY MANAGEMENT SYSTEM**";
+    int padding = (40 - strlen(title)) / 2;
+
+    int choice = 0;
+    Item items[100];
+    int count = 0;
+
+    do
+    {
+        #ifdef _WIN32                       // Clears the interface each time menu loop repeats
+            system("cls");
+        #else
+            system("clear");
+        #endif
+
+
+
+        switch (choice){
+            case 1:
+                #ifdef _WIN32
+                    system("cls");
+                #else
+                    system("clear");
+                #endif
+
+
+            break;
+
+            case 2:
+                #ifdef _WIN32
+                    system("cls");
+                #else
+                    system("clear");
+                #endif
+
+
+            break;
+
+            case 3:
+                #ifdef _WIN32
+                    system("cls");
+                #else
+                    system("clear");
+                #endif
+
+
+            break;
+
+            case 4:
+                #ifdef _WIN32
+                    system("cls");
+                #else
+                    system("clear");
+                #endif
+
+
+            break;
+
+            case 5:
+                #ifdef _WIN32
+                    system("cls");
+                #else
+                    system("clear");
+                #endif
+
+
+            break;
+
+            case 6:
+                #ifdef _WIN32
+                    system("cls");
+                #else
+                    system("clear");
+                #endif
+
+                
+            break;
+        }
+        
+        
+    } while (choice != 6);
+    
+
+
+
 
     return 0;
 }
